30321419b5232dbd6beb5c418341003c51068525
Luka Bradic <bradicluka04@gmail.com>
2024-11-20 15:36:51 +0100

    [ADD] - Add base project setup

---
+++ src/test/java/com/ftn/event_hopper/EventHopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.event_hopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
