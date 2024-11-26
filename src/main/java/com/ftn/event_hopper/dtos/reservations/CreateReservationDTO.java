d011dbf47865707b5dcaf3b62a87172966b06eef
Luka Bradic <bradicluka04@gmail.com>
2024-11-26 13:30:21 +0100

    [ADD] - Add reservations DTOs and Controller

---
+++ src/main/java/com/ftn/event_hopper/dtos/reservations/CreateReservationDTO.java
@@ -0,0 +1,18 @@
+package com.ftn.event_hopper.dtos.reservations;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateReservationDTO {
+    private UUID eventId;
+    private UUID productId;
+    private LocalDateTime from;
+    private LocalDateTime to;
+}
---
+++ src/main/java/com/ftn/event_hopper/dtos/reservations/CreatedReservationDTO.java
@@ -0,0 +1,20 @@
+package com.ftn.event_hopper.dtos.reservations;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreatedReservationDTO {
+    private UUID id;
+    private UUID eventId;
+    private UUID productId;
+    private LocalDateTime timestamp;
+    private LocalDateTime from;
+    private LocalDateTime to;
+}
