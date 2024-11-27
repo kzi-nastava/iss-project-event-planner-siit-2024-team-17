45bcf6e568c0a135a07a441d91e1db5b060ed1ca
marijaparezanin <marija.parezanin@mensa.ba>
2024-11-27 13:34:05 +0100

    [ADD] - Add location DTOs

---
+++ src/main/java/com/ftn/event_hopper/dtos/location/CreatedLocationDTO.java
@@ -0,0 +1,18 @@
+package com.ftn.event_hopper.dtos.location;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreatedLocationDTO {
+    private UUID id;
+    private String city;
+    private String address;
+    private double latitude;
+    private double longitude;
+}
