196f3138a195721250f35cfc4ba05ab389739b4d
Luka Bradic <bradicluka04@gmail.com>
2024-12-09 21:22:18 +0100

    [FIX] - Fix DTO and allow CrossOrigin

--- src/main/java/com/ftn/event_hopper/controllers/categories/CategoryController.java
+++ src/main/java/com/ftn/event_hopper/controllers/categories/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "*")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
--- src/main/java/com/ftn/event_hopper/dtos/categories/UpdateCategorySuggestionDTO.java
+++ src/main/java/com/ftn/event_hopper/dtos/categories/UpdateCategorySuggestionDTO.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class UpdateCategorySuggestionDTO {
-    private UUID id;
     private CategoryStatus status;
 }
