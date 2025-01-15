4f3101dd555bf7f1ab95215ee3af319f07d755f0
marijaparezanin <marija.parezanin@mensa.ba>
2025-01-15 12:25:43 +0100

    [MOD] - Remove access to the resend verification email end point

--- src/main/java/com/ftn/event_hopper/config/WebSecurityConfig.java
+++ src/main/java/com/ftn/event_hopper/config/WebSecurityConfig.java
@@ -86,7 +86,6 @@ public class WebSecurityConfig {
                     .requestMatchers(HttpMethod.POST, "/api/accounts/person").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/accounts/verify/*").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/events/*").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/accounts/resend-verification-email/*").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/images/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/events/*/agenda").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/images").permitAll()
