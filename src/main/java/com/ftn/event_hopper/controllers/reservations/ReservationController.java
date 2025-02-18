2190283297c9a51cde5a6dc347856d99ec4e6009
Luka Bradic <bradicluka04@gmail.com>
2025-02-18 14:21:34 +0100

    [ADD] - Implement product purchase logic

--- src/main/java/com/ftn/event_hopper/controllers/reservations/ReservationController.java
+++ src/main/java/com/ftn/event_hopper/controllers/reservations/ReservationController.java
@@ -1,47 +1,41 @@
 package com.ftn.event_hopper.controllers.reservations;
 
-import com.ftn.event_hopper.dtos.reservations.*;
+import com.ftn.event_hopper.dtos.reservations.CreateReservationProductDTO;
+import com.ftn.event_hopper.dtos.reservations.CreateReservationServiceDTO;
+import com.ftn.event_hopper.dtos.reservations.CreatedReservationProductDTO;
+import com.ftn.event_hopper.dtos.reservations.CreatedReservationServiceDTO;
+import com.ftn.event_hopper.services.reservations.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/reservations")
 public class ReservationController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<GetReservationDTO>> getReservations() {
-        Collection<GetReservationDTO> reservations = new ArrayList<GetReservationDTO>();
-
-
-        return new ResponseEntity<Collection<GetReservationDTO>>(reservations, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GetReservationDTO> getReservation(@PathVariable UUID id) {
-        GetReservationDTO reservation = new GetReservationDTO();
-
-        if (reservation == null) {
-            return new ResponseEntity<GetReservationDTO>(HttpStatus.NOT_FOUND);
-        }
-
-
-
-        return new ResponseEntity<GetReservationDTO>(reservation, HttpStatus.OK);
-    }
+    @Autowired
+    private ReservationService reservationService;
 
     @PostMapping(value = "/products", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CreatedReservationProductDTO> createReservation(@RequestBody CreateReservationProductDTO reservation) {
+    public ResponseEntity<?> createReservation(@RequestBody CreateReservationProductDTO reservation) {
+        try {
+            CreatedReservationProductDTO createdReservation = reservationService.buyProduct(reservation);
 
-        CreatedReservationProductDTO createdReservation = new CreatedReservationProductDTO();
 
-
-        return new ResponseEntity<CreatedReservationProductDTO>(createdReservation, HttpStatus.CREATED);
+            return new ResponseEntity<CreatedReservationProductDTO>(createdReservation, HttpStatus.CREATED);
+        } catch (RuntimeException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
     }
 
     @PostMapping(value = "/services", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
---
+++ src/main/java/com/ftn/event_hopper/services/reservations/ReservationService.java
@@ -0,0 +1,126 @@
+package com.ftn.event_hopper.services.reservations;
+
+import com.ftn.event_hopper.dtos.messages.NewChatMessageDTO;
+import com.ftn.event_hopper.dtos.reservations.CreateReservationProductDTO;
+import com.ftn.event_hopper.dtos.reservations.CreatedReservationProductDTO;
+import com.ftn.event_hopper.models.budgets.BudgetItem;
+import com.ftn.event_hopper.models.events.Event;
+import com.ftn.event_hopper.models.reservations.Reservation;
+import com.ftn.event_hopper.models.solutions.Product;
+import com.ftn.event_hopper.models.users.Account;
+import com.ftn.event_hopper.models.users.EventOrganizer;
+import com.ftn.event_hopper.models.users.ServiceProvider;
+import com.ftn.event_hopper.repositories.events.EventRepository;
+import com.ftn.event_hopper.repositories.reservations.ReservationRepository;
+import com.ftn.event_hopper.repositories.solutions.ProductRepository;
+import com.ftn.event_hopper.repositories.users.AccountRepository;
+import com.ftn.event_hopper.repositories.users.EventOrganizerRepository;
+import com.ftn.event_hopper.repositories.users.ServiceProviderRepository;
+import com.ftn.event_hopper.services.messages.MessageService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReservationService {
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private EventOrganizerRepository eventOrganizerRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private MessageService messageService;
+    @Autowired
+    private ServiceProviderRepository serviceProviderRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+
+
+    public CreatedReservationProductDTO buyProduct(CreateReservationProductDTO reservationRequestDTO) {
+        Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(account == null) {
+            throw new EntityNotFoundException("You must be logged in to make a reservation.");
+        }
+
+        EventOrganizer eventOrganizer = eventOrganizerRepository.findById(account.getPerson().getId())
+                .orElseThrow(() -> new EntityNotFoundException("You must be an event organizer to make a reservation."));
+
+        Event event = eventRepository.findById(reservationRequestDTO.getEventId())
+                .orElseThrow(() -> new EntityNotFoundException("Event not found."));
+
+        Product product = productRepository.findById(reservationRequestDTO.getProductId())
+                .orElseThrow(() -> new EntityNotFoundException("Product not found."));
+
+        if(eventOrganizer.getEvents().stream().noneMatch(e -> e.getId().equals(event.getId()))) {
+            throw new EntityNotFoundException("You must be the organizer of the event to make a reservation.");
+        }
+
+        if (product.getEventTypes().stream().noneMatch(et -> et.getId().equals(event.getEventType().getId()))) {
+            throw new EntityNotFoundException("Product is not available for this event type.");
+        }
+
+        ServiceProvider provider = serviceProviderRepository.findByProductsContaining(product);
+        if (provider == null) {
+            throw new EntityNotFoundException("Product provider is not reachable.");
+        }
+        Account providerAccount = accountRepository.findByPersonId(provider.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Provider account not found."));
+        if (!providerAccount.isValid()){
+            throw new EntityNotFoundException("Provider account is not valid.");
+        }
+
+
+
+        if (event.getBudgetItems().stream().noneMatch(bi -> bi.getCategory().getId().equals(product.getCategory().getId()))) {
+            BudgetItem budgetItem = new BudgetItem();
+            budgetItem.setCategory(product.getCategory());
+            budgetItem.setAmount(0);
+            budgetItem.setId(null);
+            event.getBudgetItems().add(budgetItem);
+            eventRepository.save(event);
+            eventRepository.flush();
+        }
+
+        Reservation newReservation = new Reservation();
+        newReservation.setEvent(event);
+        newReservation.setProduct(product);
+        newReservation.setTimestamp(LocalDateTime.now());
+        Reservation created = reservationRepository.save(newReservation);
+        reservationRepository.flush();
+        CreatedReservationProductDTO ret = new CreatedReservationProductDTO();
+        ret.setId(created.getId());
+        ret.setEventId(created.getEvent().getId());
+        ret.setProductId(created.getProduct().getId());
+        ret.setTimestamp(created.getTimestamp());
+
+
+
+        this.notifyOfReservation(account, providerAccount, this.generateProductReservationMessage(event, product, account));
+
+        return ret;
+    }
+
+    private String generateProductReservationMessage(Event event, Product product, Account account) {
+        return String.format(
+                "Dear %s,\n Thank you for purchasing our product %s for the event %s. We hope you enjoy it!",
+                account.getUsername(),
+                product.getName(),
+                event.getName()
+        );
+    }
+
+
+    private void notifyOfReservation(Account account, Account providerAccount, String messageText) {
+        NewChatMessageDTO message = new NewChatMessageDTO();
+        message.setRecipient(account.getUsername());
+        message.setSender(providerAccount.getUsername());
+        message.setContent(messageText);
+        messageService.sendMessage(message);
+    }
+}
