cd1d777fc9cdae20cb2ed513f4a9fbb770d7994b
marijaparezanin <marija.parezanin@mensa.ba>
2025-06-10 00:30:27 +0200

    [FIX] - Remove unique constraint for product name

--- src/main/java/com/ftn/event_hopper/models/solutions/Product.java
+++ src/main/java/com/ftn/event_hopper/models/solutions/Product.java
@@ -30,7 +30,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(length = 1000)
