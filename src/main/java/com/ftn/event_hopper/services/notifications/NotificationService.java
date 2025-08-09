0ee35d183d0bfbc7b611b836d2aa11052541c75e
Vanja Kostic <vanjakostic03@gmail.com>
2025-08-09 19:59:49 -0400

    [ADD] - sending notifications in realtime

--- src/main/java/com/ftn/event_hopper/services/notifications/NotificationService.java
+++ src/main/java/com/ftn/event_hopper/services/notifications/NotificationService.java
@@ -5,12 +5,15 @@ import com.ftn.event_hopper.dtos.notifications.SimpleNotificationDTO;
 import com.ftn.event_hopper.models.events.Event;
 import com.ftn.event_hopper.models.notifications.Notification;
 import com.ftn.event_hopper.models.solutions.Product;
+import com.ftn.event_hopper.models.users.Account;
 import com.ftn.event_hopper.models.users.Person;
 import com.ftn.event_hopper.repositories.events.EventRepository;
 import com.ftn.event_hopper.repositories.notifications.NotificationRepository;
 import com.ftn.event_hopper.repositories.solutions.ProductRepository;
+import com.ftn.event_hopper.repositories.users.AccountRepository;
 import com.ftn.event_hopper.repositories.users.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -27,6 +30,10 @@ public class NotificationService {
     private ProductRepository productRepository;
     @Autowired
     private EventRepository eventRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
 
     public boolean sendNotification(CreateNotificationDTO createNotificationDTO,UUID eventId, UUID productID, UUID personId) {
@@ -52,6 +59,13 @@ public class NotificationService {
         if (person != null) {
             person.getNotifications().add(notification);
             personRepository.save(person);
+            Account account = accountRepository.findByPersonId(personId).orElse(null);
+            if (account == null) {
+                return false;
+            }
+
+            messagingTemplate.convertAndSendToUser(account.getUsername(),"/topic/notifications", notification);
+
             return true;
         }
 
